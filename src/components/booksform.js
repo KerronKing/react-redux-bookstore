7ee42174354ac761a23d67ca771e5e6926275260
KerronKing <king_of_palmiste@hotmail.com>
2020-02-20 14:11:23 -0400

    feat: add handleChange, handleSubmit and dispatch mapping functions to booksform component

--- src/components/booksform.js
+++ src/components/booksform.js
@@ -1,32 +1,79 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import Proptypes from 'prop-types';
+import { createAction } from '../actions/index';
 
-const BooksForm = () => {
-  const bookCategories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-  return (
-    <form>
-      <fieldset className="form-group">
-        <label htmlFor="formInput">
-          title
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Enter a book Title"
-            id="formInput"
-          />
-        </label>
-      </fieldset>
-      <fieldset>
-        <select>
-          {bookCategories.map(category => (
-            <option key={category} value={category}>
-              {category}
-            </option>
-          ))}
-        </select>
-      </fieldset>
-      <button type="submit">Submit</button>
-    </form>
-  );
+class BooksForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      category: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    if (e.target.id === 'formInput') {
+      this.setState({
+        title: e.target.value,
+      });
+    } else if (e.target.id === 'categories') {
+      this.setState({
+        category: e.target.value,
+      });
+    }
+    console.log(e.target.id);
+  }
+
+  handleSubmit() {
+    const { addBook } = this.props;
+    addBook(createAction);
+    this.setState({
+      title: '',
+      category: '',
+    });
+    console.log('submitted');
+  }
+
+  render() {
+    const bookCategories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+    return (
+      <form>
+        <fieldset className="form-group">
+          <label htmlFor="formInput">
+            title
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter a book Title"
+              id="formInput"
+              onChange={this.handleChange}
+            />
+          </label>
+        </fieldset>
+        <fieldset>
+          <select id="categories" onClick={this.handleChange}>
+            {bookCategories.map(category => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </fieldset>
+        <button type="submit" onSubmit={this.handleSubmit}>Submit</button>
+      </form>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  addBook: () => dispatch(createAction()),
+});
+
+BooksForm.propTypes = {
+  addBook: Proptypes.func.isRequired,
 };
 
-export default BooksForm;
+export default connect(null, mapDispatchToProps)(BooksForm);
