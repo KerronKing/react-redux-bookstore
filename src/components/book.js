4e51953b99335e4879ac94744c8613270049740d
Roy Nyaga <nyagaandreroy@gmail.com>
2020-02-21 21:56:24 +0100

    added new table column to BookList and handleRemoveBook click event

--- src/components/book.js
+++ src/components/book.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import Proptypes from 'prop-types';
+import React from 'react'
+import Proptypes from 'prop-types'
 
-const Book = props => {
-  const { bookObject } = props;
-  return (
-    <tr>
-      <td>{bookObject.id}</td>
-      <td>{bookObject.title}</td>
-      <td>{bookObject.category}</td>
-    </tr>
-  );
-};
+class Book extends React.Component {
+  constructor (props) {
+    super(props)
+    this.removeBook = this.removeBook.bind(this)
+  }
+
+  removeBook () {
+  	const { bookObject, handleRemoveBook } = this.props
+  	handleRemoveBook(bookObject)
+  }
+
+  render () {
+    const { bookObject, handleRemoveBook } = this.props
+    return (
+      <tr>
+        <td>{bookObject.id}</td>
+        <td>{bookObject.title}</td>
+        <td>{bookObject.category}</td>
+        <td><button onClick={this.removeBook} type='button'>Remove Book</button></td>
+      </tr>
+		  )
+  }
+}
 
 Book.propTypes = {
-  bookObject: Proptypes.instanceOf(Object).isRequired,
-};
+  bookObject: Proptypes.instanceOf(Object).isRequired
+}
 
-export default Book;
+export default Book
