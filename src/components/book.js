82de2353cede7349eff0c9f2ca4678bb617adc3b
Roy Nyaga <nyagaandreroy@gmail.com>
2020-02-27 18:27:47 +0100

    styled table rows and navbar

--- src/components/book.js
+++ src/components/book.js
@@ -1,33 +1,35 @@
-import React from 'react';
-import Proptypes from 'prop-types';
+import React from 'react'
+import Proptypes from 'prop-types'
 
 class Book extends React.Component {
-  constructor(props) {
-    super(props);
-    this.removeBook = this.removeBook.bind(this);
+  constructor (props) {
+    super(props)
+    this.removeBook = this.removeBook.bind(this)
   }
 
-  removeBook() {
-    const { bookObject, handleRemoveBook } = this.props;
-    handleRemoveBook(bookObject);
+  removeBook () {
+    const { bookObject, handleRemoveBook } = this.props
+    handleRemoveBook(bookObject)
   }
 
-  render() {
-    const { bookObject } = this.props;
+  render () {
+    const { bookObject } = this.props
     return (
-      <tr>
-        <td>{bookObject.id}</td>
-        <td>{bookObject.title}</td>
-        <td>{bookObject.category}</td>
-        <td><button onClick={this.removeBook} type="button">Remove Book</button></td>
+      <tr className='pl-5 table-row'>
+        <td className='table-row pl-5'>
+          <p className='table-category'>{bookObject.category}</p>
+          <p className='table-title'>{bookObject.title}</p>
+          <p className='table-remove' onClick={this.removeBook}>Remove Book</p>
+        </td>
+
       </tr>
-    );
+    )
   }
 }
 
 Book.propTypes = {
   bookObject: Proptypes.instanceOf(Object).isRequired,
-  handleRemoveBook: Proptypes.func.isRequired,
-};
+  handleRemoveBook: Proptypes.func.isRequired
+}
 
-export default Book;
+export default Book
--- src/containers/bookslist.js
+++ src/containers/bookslist.js
@@ -1,48 +1,40 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import Proptypes from 'prop-types';
-import Book from '../components/book';
-import { removeAction, changeFilter } from '../actions/index';
-import CategoryFilter from '../components/categoryFilter';
+import React from 'react'
+import { connect } from 'react-redux'
+import Proptypes from 'prop-types'
+import Book from '../components/book'
+import { removeAction, changeFilter } from '../actions/index'
+import CategoryFilter from '../components/categoryFilter'
 
 class Bookslist extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
 
-    this.handleRemoveBook = this.handleRemoveBook.bind(this);
-    this.handleFilterChange = this.handleFilterChange.bind(this);
+    this.handleRemoveBook = this.handleRemoveBook.bind(this)
+    this.handleFilterChange = this.handleFilterChange.bind(this)
   }
 
-  handleRemoveBook(bookObject) {
-    const { removeBook } = this.props;
-    removeBook(bookObject);
+  handleRemoveBook (bookObject) {
+    const { removeBook } = this.props
+    removeBook(bookObject)
   }
 
-  handleFilterChange(category) {
-    const { filterBook } = this.props;
-    filterBook(category);
+  handleFilterChange (category) {
+    const { filterBook } = this.props
+    filterBook(category)
   }
 
-  render() {
-    const { books, filter } = this.props;
-    let filteredBooks;
+  render () {
+    const { books, filter } = this.props
+    let filteredBooks
     if (filter === '') {
-      filteredBooks = books;
+      filteredBooks = books
     } else {
-      filteredBooks = books.filter(book => book.category === filter);
+      filteredBooks = books.filter(book => book.category === filter)
     }
 
     return (
-      <div>
-        <table className="table table-inverse">
-          <thead>
-            <tr>
-              <th>Book id</th>
-              <th>Title</th>
-              <th>Category</th>
-              <th>Remove</th>
-            </tr>
-          </thead>
+      <div className='container-fluid mt-5 table-container'>
+        <table className='table table-inverse'>
           <tbody>
             {
               filteredBooks.map(book => (
@@ -57,24 +49,24 @@ class Bookslist extends React.Component {
         </table>
         <CategoryFilter handleFilterChange={this.handleFilterChange} />
       </div>
-    );
+    )
   }
 }
 
 const mapStateToProps = state => ({
   books: state.book.books,
-  filter: state.filtrate.filter,
-});
+  filter: state.filtrate.filter
+})
 const mapDispatchToProps = dispatch => ({
   removeBook: bookObject => dispatch(removeAction(bookObject)),
-  filterBook: category => dispatch(changeFilter(category)),
-});
+  filterBook: category => dispatch(changeFilter(category))
+})
 
 Bookslist.propTypes = {
   books: Proptypes.instanceOf(Object).isRequired,
   filter: Proptypes.string.isRequired,
   removeBook: Proptypes.func.isRequired,
-  filterBook: Proptypes.func.isRequired,
-};
+  filterBook: Proptypes.func.isRequired
+}
 
-export default connect(mapStateToProps, mapDispatchToProps)(Bookslist);
+export default connect(mapStateToProps, mapDispatchToProps)(Bookslist)
