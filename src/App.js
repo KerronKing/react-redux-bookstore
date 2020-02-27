786fbaf75aa2c4bc94536a452dfd7572668c475b
KerronKing <king_of_palmiste@hotmail.com>
2020-02-27 16:02:21 -0400

    style: style filter form

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import Bookslist from './containers/bookslist'
-import Booksform from './containers/booksform'
+import React from 'react';
+import Bookslist from './containers/bookslist';
+import Booksform from './containers/booksform';
 
-import './app.css'
+import './app.css';
 
 const App = () => (
-  <div className='App'>
-    <div className='container-fluid px-5 bg-white'>
-      <div className='row'>
-        <div className='col-12 d-flex justify-content-between'>
-          <div id='nav-left' className='d-flex'>
-            <h1 className='p-4 text-primary' id='book-store-h1'>Bookstore CMS</h1>
-            <h6 className='p-4' id='books-h5'>BOOKS</h6>
-            <h6 className='p-4' id='categories-h5'>CATEGORIES</h6>
+  <div className="App">
+    <div className="container-fluid px-5 bg-white">
+      <div className="row">
+        <div className="col-12 d-flex justify-content-between">
+          <div id="nav-left" className="d-flex">
+            <h1 className="p-4 text-primary" id="book-store-h1">Bookstore CMS</h1>
+            <h6 className="p-4" id="books-h5">BOOKS</h6>
+            <h6 className="p-4" id="categories-h5">CATEGORIES</h6>
           </div>
-          <div id='nav-right'>
-            <p className='mt-4'><i className='user-icon fas fa-user' /></p>
+          <div id="nav-right">
+            <p className="mt-4"><i className="user-icon fas fa-user" /></p>
           </div>
         </div>
       </div>
@@ -23,6 +23,6 @@ const App = () => (
     <Bookslist />
     <Booksform />
   </div>
-)
+);
 
-export default App
+export default App;
--- src/components/book.js
+++ src/components/book.js
@@ -1,35 +1,35 @@
-import React from 'react'
-import Proptypes from 'prop-types'
+import React from 'react';
+import Proptypes from 'prop-types';
 
 class Book extends React.Component {
-  constructor (props) {
-    super(props)
-    this.removeBook = this.removeBook.bind(this)
+  constructor(props) {
+    super(props);
+    this.removeBook = this.removeBook.bind(this);
   }
 
-  removeBook () {
-    const { bookObject, handleRemoveBook } = this.props
-    handleRemoveBook(bookObject)
+  removeBook() {
+    const { bookObject, handleRemoveBook } = this.props;
+    handleRemoveBook(bookObject);
   }
 
-  render () {
-    const { bookObject } = this.props
+  render() {
+    const { bookObject } = this.props;
     return (
-      <tr className='pl-5 table-row'>
-        <td className='table-row pl-5'>
-          <p className='table-category'>{bookObject.category}</p>
-          <p className='table-title'>{bookObject.title}</p>
-          <p className='table-remove' onClick={this.removeBook}>Remove Book</p>
+      <tr className="pl-5 table-row">
+        <td className="table-row pl-5">
+          <p className="table-category">{bookObject.category}</p>
+          <p className="table-title">{bookObject.title}</p>
+          <button type="submit" className="table-remove" onClick={this.removeBook}>Remove Book</button>
         </td>
 
       </tr>
-    )
+    );
   }
 }
 
 Book.propTypes = {
   bookObject: Proptypes.instanceOf(Object).isRequired,
-  handleRemoveBook: Proptypes.func.isRequired
-}
+  handleRemoveBook: Proptypes.func.isRequired,
+};
 
-export default Book
+export default Book;
